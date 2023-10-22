522ce42a2cb8eb53a31e66399b543868627a0db3
JP511ale <jeobando@estudiantec.cr>
2023-10-21 19:19:30 -0600

    Conexion con la base

---
+++ ConexionBD.py
@@ -0,0 +1,26 @@
+import couchdb
+
+couchdb_server_url = "http://admin:admin@localhost:5984" #Aquí cambian por sus credenciales
+server = couchdb.Server(couchdb_server_url)
+
+db_name = "replica_proyectobda"  
+db = server[db_name]
+
+# Ejemplo de inserción de un documento
+doc = {
+    "nombre": "Jeanpool Obando Calvo",
+    "puesto": "Scrum Master",
+    "departamento": "Proyectos",
+    "esInternacional": "Sí",
+    "paisDestino": "Portugal",
+    "motivoViaje": "Trabajo",
+    "fechaIncio": "10/08/2023",
+    "fechaFinalización": "12/11/2023",
+    "detallesVuelos": {
+        "aerolinea": "Avianca",
+        "precioBoletos": 650000
+    },
+    "alojamiento": "Four seasons",
+    "requiereTransporte": "No"
+}
+db.save(doc)
